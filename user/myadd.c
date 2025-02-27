9f0c52e6804a4f634cc18f5f61b1135fba2a4696
Sanya239 <alex.bogdanov05@mail.ru>
2025-02-27 10:29:56 +0300

    some fixes

--- user/myadd.c
+++ user/myadd.c
@@ -1,20 +1,27 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int max_figure_size = 11;
+const int max_figure_size = 11;
+const int max_input_size = 2 * (max_figure_size + 1);
 
 void fileread_error() {
-    fprintf(2,"%s", "unexpected error\n");
+    fprintf(2, "%s", "unexpected error\n");
+}
+
+void overflow_error() {
+    fprintf(2, "%s", "Input is too long\n");
+
 }
 
 void format_error() {
-    fprintf(2,"%s", "incorrect format\n");
+    fprintf(2, "%s", "incorrect format\n");
 }
 
-void read_chars(char *buff) {
+void read_chars(char *buff, int size) {
     char *p = buff;
+    int count = 0;
     int cc;
-    while ((cc = read(0, p, 1))) {
+    while (count++ < size && (cc = read(0, p, 1))) {
         if (cc == -1) {
             fileread_error();
         }
@@ -36,7 +43,7 @@ int parse_int(char *buff) {
 int validate(char *buff) {
 
     int p = 0;
-    if(buff[p]=='-'){
+    if (buff[p] == '-') {
         p++;
     }
     while (buff[p] != ' ') {
@@ -48,30 +55,31 @@ int validate(char *buff) {
     }
     int whitespace_pos = p;
     p++;
-    if(buff[p]=='-'){
+    if (buff[p] == '-') {
         p++;
     }
-    while (buff[p] != 0 && buff[p] != '\n' && buff[p] != '\r') {
+    while (buff[p] != 0 && buff[p] != '\n') {
         if (buff[p] > '9' || buff[p] < '0') {
             format_error();
             return -1;
         }
         p++;
     }
+    buff[p] = 0;
     return whitespace_pos;
 }
 
 int
 main(int argc, char *argv[]) {
-    char buff[max_figure_size * 2 + 2];
-    read_chars(buff);
+    char buff[max_input_size];
+    read_chars(buff, max_input_size);
     if (argc == 2) {
         if (strcmp(argv[1], "--test") == 0) {
             printf("|%s|\n", buff);
         }
     }
     int whitespace_pos = validate(buff);
-    if(whitespace_pos == -1){
+    if (whitespace_pos == -1) {
         return 1;
     }
     int num1 = parse_int(buff);
