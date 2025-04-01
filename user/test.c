7e85572f6c94bed1876e85336429c192255eeaf9
Sanya239 <alex.bogdanov05@mail.ru>
2025-04-02 02:14:40 +0300

    that was not easy

---
+++ user/test.c
@@ -0,0 +1,24 @@
+#include "kernel/types.h"
+
+#include "user/user.h"
+
+int main() {
+    printf("before mallocs\n");
+    pagemap(0, 0, 0);
+    char *bebebe = (char *) malloc(40000);
+    char buf[2000];
+    printf("with memory allocated\n");
+    pagemap(buf, 0, 0);
+    bebebe[0] = 'b';
+    bebebe[10000] = 'e';
+    bebebe[20000] = bebebe[30000];
+    printf("with memory accessed\n");
+    pagemap(bebebe, 40000, 3);
+    delete_DA(0, 0, 3);
+    printf("with memory cleaned\n");
+    pagemap(bebebe, 40000, 3);
+    free(bebebe);
+    printf("after free\n");
+    pagemap(0, 0, 0);
+    exit(0);
+}
--- user/usys.pl
+++ user/usys.pl
@@ -36,3 +36,5 @@ entry("getpid");
 entry("sbrk");
 entry("sleep");
 entry("uptime");
+entry("pagemap");
+entry("delete_DA");
